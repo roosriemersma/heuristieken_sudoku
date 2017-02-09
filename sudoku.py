fc539edf8ff7cfc262f2060b549df2a5d5f85fb0
RoosRiemersma <roos.riemersma@hotmail.com>
2017-02-09 19:47:35 +0100

    Ingelezen file

--- sudoku.py
+++ sudoku.py
@@ -1,7 +1,4 @@
-print ("Hello world!")
-print ("Hello bor!")
-print ("Hello Roos!!! <3 <3 <3")
-print ("Let's get started guys")
+
 7,9,0,0,0,0,3,0,1
 0,0,0,0,0,6,9,0,0
 8,0,0,0,3,0,0,7,6
@@ -11,5 +8,6 @@ print ("Let's get started guys")
 6,1,0,0,9,0,0,0,8
 0,0,2,3,0,0,0,0,0
 0,0,9,0,0,0,0,5,4
-print ("Wow Rose, you're the best!")
-print ("Hello")
+#file inlezen
+filename = 'puzzle1.sudoku'
+sudokuEmpty = open(filename, mode='r')
