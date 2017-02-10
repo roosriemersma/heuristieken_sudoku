f21f6a24387de27483a3d242aab6cfe19438f195
RoosRiemersma <roos.riemersma@hotmail.com>
2017-02-10 10:00:20 +0100

    Listjes in de list

--- sudoku.py
+++ sudoku.py
@@ -1,16 +1,16 @@
 #list filled with list of every line in sudoku
-row = []
-sudoku = open("puzzle1.sudoku", "r")
-for line in sudoku:
+sudoku = []
+puzzle = open("puzzle1.sudoku", "r")
+for line in puzzle:
     line = line.replace(",", "")
     line = line.rstrip()
     line = list(line)
     line = list(map(int, line))
-    row.append(line)
-print (row)
+    sudoku.append(line)
+print (sudoku)
 
 #geprint als matrix, weet niet of je hier iets mee kan :)
 
-for stukje in row:
-    print (stukje)
+for row in sudoku:
+    print (row)
 
