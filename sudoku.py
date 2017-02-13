7bed83db143fe86c3d38f7cedb08ff0b89779b80
RoosRiemersma <roos.riemersma@hotmail.com>
2017-02-13 17:18:59 +0100

    Print sudoku

--- sudoku.py
+++ sudoku.py
@@ -11,9 +11,17 @@ print (sudoku)
 
 #geprint als matrix, weet niet of je hier iets mee kan :)
 
-for row in sudoku:
-    print (row)
+#for row in sudoku:
+#    print (row)
 
 #kolom geprint, waat ook niet of we hier iets mee kunnen, loop maken lukt me niet
-for firstNumb in sudoku:
-    print (firstNumb[0:1])
+#for firstNumb in sudoku:
+#    print (firstNumb[0:1])
+
+#printen van de sudoku
+def print_sudoku(x):
+    for i in range(9):
+        for j in range(9):
+            print (x[i][j]),
+        print ('\n')
+
