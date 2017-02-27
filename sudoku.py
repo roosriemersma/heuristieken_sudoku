2d4f2cae37c1667141bb4ad0f49e33ef6deb430c
MaartjeMolenaar <maartjemolenaar1997@gmail.com>
2017-02-27 18:05:35 +0100

    FINAL

--- sudoku.py
+++ sudoku.py
@@ -1,6 +1,10 @@
+RANGE = 9
+BOX_RANGE = 3
+FIRST_LINE = 2
+SECOND_LINE = 5
 #list filled with list of every line in sudoku
 sudoku = []
-puzzle = open("puzzle6.sudoku", "r")
+puzzle = open("puzzle1.sudoku", "r")
 for line in puzzle:
     line = line.replace(",", "")
     line = line.rstrip()
@@ -10,23 +14,23 @@ for line in puzzle:
 
 #printen van de sudoku
 def sudoku_printen(input):
-    for i in range(9):
-        for j in range(9):
-            if j == 2:
+    for i in range(RANGE):
+        for j in range(RANGE):
+            if j == FIRST_LINE:
                 print (input[i][j], "|", end=" ")
-            elif j == 5:
+            elif j == SECOND_LINE:
                 print(input[i][j], "|", end=" ")
             else:
                 print (input[i][j], end=" ")
         print ('\n', end="")
-        if i == 2 or i == 5:
-            print("- - - + - - -+ - - -")
+        if i == FIRST_LINE or i == SECOND_LINE:
+            print("- - - + - - - + - - -")
 
 #zoeken naar lege plek in sudoku
 def zoek_lege_plek(input, plek):
 
-    for rij in range(9):
-        for kolom in range(9):
+    for rij in range(RANGE):
+        for kolom in range(RANGE):
             if(input[rij][kolom] == 0):
                 plek[0] = rij
                 plek[1] = kolom
@@ -37,22 +41,22 @@ def zoek_lege_plek(input, plek):
 
 #zoeken of nummer gebruikt wordt in de rij, als nummer in rij zit dan return True
 def zit_in_rij(input, rij, nummer):
-    for i in range(9):
+    for i in range(RANGE):
         if (input[rij][i] == nummer):
             return True
     return False
 
 #zoeken of nummer wordt gebruikt in kolom, als nummer in kolom zit dan return True
 def zit_in_kolom(input, kolom, nummer):
-    for i in range (9):
+    for i in range (RANGE):
         if (input[i][kolom] == nummer):
             return True
     return False
 
 #zoeken of nummer wordt gebruikt in box, als nummer in box zit dan return True
 def zit_in_box(input, rij, kolom, nummer):
-    for i in range (3):
-        for j in range(3):
+    for i in range (BOX_RANGE):
+        for j in range(BOX_RANGE):
             if (input[i + rij][j + kolom] == nummer):
                 return True
     return False
